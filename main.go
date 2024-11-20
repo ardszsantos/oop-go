a38ab7af8fabb01fb71df9e754fca1371e8e2e7a
ardszsantos <lokgamer01@gmail.com>
2024-11-20 10:32:33 -0300

    funcionando

--- main.go
+++ main.go
@@ -9,14 +9,25 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+
+}
+
 func main() {
 	contaDaSilvia := ContaCorrente{}
 	contaDaSilvia.titular = "Silvia"
 	contaDaSilvia.saldo = 500
 
+	fmt.Println(contaDaSilvia.Sacar(-600))
+
 	fmt.Println(contaDaSilvia.saldo)
 
-	valorDoSaque := 200.
-
-	contaDaSilvia.saldo = contaDaSilvia.saldo - valorDoSaque
 }
