2ba4ba064c6d26803220c84f87c3523ab58b3a2d
ardszsantos <lokgamer01@gmail.com>
2024-11-20 10:01:44 -0300

    feat: usando structs

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ type ContaCorrente struct {
 
 func main() {
 
-	var contaDoGuilherme ContaCorrente = ContaCorrente{
-		titular: "Guilherme",
-		saldo:   400.50,
+	contaDoGuilherme := ContaCorrente{
+		titular:       "Guilherme",
+		numeroAgencia: 123456,
+		numeroConta:   12321412,
+		saldo:         400.50,
 	}
 
-	fmt.Println("Conta do:", contaDoGuilherme.titular, "\n", "Saldo: ", contaDoGuilherme.saldo)
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 11222}
+
+	fmt.Println("Conta do:", contaDoGuilherme.titular, "\n", "Saldo: ", contaDoGuilherme.saldo, " mas fds o da ")
 }
