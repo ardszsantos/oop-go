96d591300053adec0bd1c7015699c7fd12093bb8
ardszsantos <lokgamer01@gmail.com>
2024-11-30 14:13:34 -0300

    removed saldo from here

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"oop-go/clientes"
 	"oop-go/contas"
 )
 
 func main() {
-	clienteBruno := clientes.Titular{"Bruno", "123.123.123.12", "Desenvolvedor go"}
-	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 12314, 100}
+	contaExemplo := contas.ContaCorrente{}
 
-	fmt.Println(contaDoBruno)
+	fmt.Println(contaExemplo)
 }
