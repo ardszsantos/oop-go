8257070c74c23cea58e2225da66b3951407f16a9
ardszsantos <lokgamer01@gmail.com>
2024-11-30 14:08:20 -0300

    testing

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
+	"oop-go/clientes"
 	"oop-go/contas"
 )
 
 func main() {
-	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
-	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
-	fmt.Println(contaDaSilvia, contaDoGustavo)
+	contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
+		Nome:      "Bruno",
+		CPF:       "431.525.324.53",
+		Profissao: "Joalheiro",
+	}, NumeroAgencia: 123, NumeroConta: 123456, Saldo: 100}
 
-	status := contaDaSilvia.Transferir(200, &contaDoGustavo)
-
-	fmt.Println("A transferencia foi concluida?", status)
-
-	fmt.Println(contaDaSilvia, contaDoGustavo)
-
+	fmt.Println(contaDoBruno)
 }
