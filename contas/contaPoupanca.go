55e7a0768bbf8c4d29d7391f98657a5750dc56ec
ardszsantos <lokgamer01@gmail.com>
2024-12-01 12:34:02 -0300

    copiei da corrente

--- contas/contaPoupanca.go
+++ contas/contaPoupanca.go
@@ -15,3 +15,27 @@ func main() {
 
 	fmt.Println("todes")
 }
+
+func (c *ContaPoupanca) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
+}
+
+func (c *ContaPoupanca) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso.", c.saldo
+	} else {
+		return "Valor do deposito menor que zero", c.saldo
+	}
+}
+
+func (c *ContaPoupanca) ObterSaldo() float64 {
+	return c.saldo
+}
